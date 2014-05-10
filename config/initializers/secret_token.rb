962fcbabb318d21e22f6e70da7a38c7642adfee3
Kenneth Yu <kenyu@ucdavis.edu>
2014-05-10 12:16:43 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccountabilityBuddy::Application.config.secret_token = '422422f5be36f01248d73de02105233b27857f040540beaa085d10c2c9820e5bb1adcbf55976b24456e96c4d78d2324c77e01cdde839bf699843fcc63fe5f945'
